05ef6a99664233b6ec40f3e7afe9bee8303990ea
ChenSingYo <chensingyo@gmail.com>
2021-02-08 12:45:29 +0800

    fix: refine login.js

--- routes/modules/login.js
+++ routes/modules/login.js
@@ -2,17 +2,23 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 
+// 如果重新載入login頁面，會回到首頁路由，然後進到success頁面
 router.get('/', (req, res) => {
   res.redirect('/')
 })
 
+// 送出帳號密碼核對，確認後取得session
 router.post('/', (req, res) => {
+  // 取得表單中的資料
   const toLogin = req.body
   User.find().lean()
     .then(users => {
+    // 比對資料庫中是否有資料符合表單內容
+
       const validUser = users.find(user =>
         toLogin.email === user.email && toLogin.password === user.password
       )
+      // 如果使用者驗證成功，則賦予session維持狀態，然後在session中加入name和userId，方便後續使用
       if (validUser) {
         req.session.name = validUser.name
         req.session.userId = validUser._id
@@ -23,7 +29,7 @@ router.post('/', (req, res) => {
     })
 })
 
-router.post('/failure', (req, res) => {
+router.post('/try_again', (req, res) => {
   res.redirect('/')
 })
 
